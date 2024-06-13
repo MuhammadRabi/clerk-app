be987eec2d187a630d1dcb46c33bd835cb66f329
Muhammad Rabi <vonrabi07@yahoo.com>
2024-06-13 14:25:13 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+export default function Home() {
+  return (
+    <main className='h-screen'>
+      <div className='rounded-lg border-4 border-red-100 py-24'>
+        <h1 className='my-12 bg-gradient-to-r from-sky-400 to-emerald-700 bg-clip-text p-4 text-center text-4xl font-bold text-transparent md:text-6xl'>
+          This is a template for Next.js Projects
+        </h1>
+        <article className='mx-auto w-full  shadow-lg lg:w-3/4 '>
+          <p className='rounded-md border border-sky-400/25 bg-slate-100/75 p-4 leading-8 text-slate-800 max-md:text-sm max-md:leading-7'>
+            Thyra surveyed the water around her, diligently waiting for a minnow
+            to reveal itself between the soft glints of reflected sunlight. Her
+            long skirt was knotted around her waist, serving two purposes –
+            keeping it dry and holding in place her bilberry-laden pouch. A
+            faint ripple appeared beside her left foot. Without hesitation,
+            Thyra deftly plucked a small silvery fish from the brook. She had
+            four now, which wasn’t a lot, but with a portion of bread and a
+            handful of berries, it would be sufficient. Cadoc was not expected
+            home for another few days, so she only needed just enough for
+            herself. Well, almost just herself. Thyra smiled as she felt the
+            newly familiar flutters of their growing child.ur hic quo natus
+            provident fugiat recusandae. Facere, ex natus?
+          </p>
+        </article>
+      </div>
+    </main>
+  )
+}
